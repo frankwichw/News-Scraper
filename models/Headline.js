4a42640dd23d8c6d38a84f48ececa050a89d3224
frankwichw <frankwichw@gmail.com>
2018-03-27 14:45:01 -0400

    handlebars added

---
+++ models/Headline.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+// referencing schema constructor
+var Schema = mongoose.Schema;
+
+// using schema constructor to make model of article documents
+var ArticleConstructor = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  summary: {
+    type: String,
+    required: true
+  },
+  url: {
+      type: String,
+      required: true
+  },
+  // note store the note id from the note documents
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// creating model from above schema
+var Article = mongoose.model("Article", ArticleConstructor);
+
+// Export the Article model
+module.exports = Article;
---
+++ models/Note.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+// referencing schema constructor
+var Schema = mongoose.Schema;
+
+// using schema constructor to make model of note documents
+var NoteConstructor = new Schema({
+  body: {
+      type: String,
+      required: true
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Note = mongoose.model("Note", NoteConstructor);
+
+// Export the Note model
+module.exports = Note;
